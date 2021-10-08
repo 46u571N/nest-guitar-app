4efc2f0af83a3b5d78941168653c2b6960994668
46u571N <leo.gardey@gmail.com>
2021-10-08 12:40:34 -0300

    Crear Entidad Guitar [Close #4]

--- src/guitars/entities/guitar.entity.ts
+++ src/guitars/entities/guitar.entity.ts
@@ -1,1 +1,9 @@
-export class Guitar {}
+export class Guitar {
+
+    private id: number;
+    private name: string;
+    private desciption: string;
+    private price: number;
+    private idmarca: number;
+}
+
