e8cdc5641607d066a0ad03c10dd4921a42e3e2b4
46u571N <leo.gardey@gmail.com>
2022-11-10 15:35:04 -0300

    modificando test duplicado y brands.service

---
+++ src/brands/brands.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
+
+@Injectable()
+export class BrandsService {
+  
+  constructor(
+    @InjectRepository(Brand)
+    private brandRepository: Repository<Brand>
+  ) { }
+
+  create(createBrandDto: CreateBrandDto) {
+    const brand: Brand = this.brandRepository.create(createBrandDto);
+    return this.brandRepository.save(brand);
+  }
+
+  findAll() {
+    return this.brandRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.brandRepository.findOneOrFail(id);
+  }
+
+  update(id: number, updateBrandDto: UpdateBrandDto) {
+    this.brandRepository.update(id, updateBrandDto);
+    return this.brandRepository.findOne(id);
+  }
+
+  remove(id: number) {
+    this.brandRepository.delete(id);
+    return `This action removes a #${id} brand`;
+  }
+}
